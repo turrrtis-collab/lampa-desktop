668243a5ac9f7aa62471975f601b4bb6d1412a4d
turrrtis-collab <turrrtis@gmail.com>
2026-04-07 13:33:31 +0300

    fix preload context and jquery init

--- src/preload.js
+++ src/preload.js
@@ -1,7 +1,11 @@
-const { contextBridge } = require('electron');
 const fs = require('fs');
 const { spawn } = require('child_process');
-contextBridge.exposeInMainWorld('api', {
+
+window.api = {
     fileExists: (path) => fs.existsSync(path),
-    spawnProcess: (path, args) => spawn(path, args),
-});
+    spawnProcess: (path, args) => spawn(path, args)
+};
+
+window.nodeModule = window.module;
+delete window.module;
+delete window.exports;
